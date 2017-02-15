f4431514862eff1202fce7812105c29319d3499c
fidelcoria <fxc140330@utdallas.edu>
2017-02-15 10:39:58 -0600

    Added comments; one example of usage

--- SBoard.h
+++ SBoard.h
@@ -4,15 +4,18 @@
 class SBoard {
 
 private:
+    //all private attributes have a '_' prefix
+
     int _filledCells;   //counts number of occupied cells (non-zero)
 
 	int _cell[9][9];    //represents 9x9 sudoku board
 
 	bool _isClue[9][9]; //table to identify clues and non-clues (guess cells)
 
-	bool _isInHouse[9][9];  //tables reporting values present (whether as clues or guesses)
-	bool _isInRow  [9][9];  //in each house, row, and column (each numbered 0-8)
-	bool _isInCol  [9][9];
+	bool _isInHouse[9][9];  //tables reporting values present (includes clues and guesses)
+	bool _isInRow  [9][9];  //the first index is th house, row, and column (each numbered 0-8)
+	bool _isInCol  [9][9];  //the second index is the value (minus one)
+	//ex. "does row 4 have the number 8?" ==> _isInRow[4][8-1]
 
 	bool isClue(int r, int c);  //getter methods
     bool rowHas(int r, int n);
